b92f6e30590e7c2856e4d9b6822bcc3b035d193b
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-05 14:54:11 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
 color 3
 
-if %1==windows (
+if %1==nodejs (
   md hxml
   cd hxml
-  fsutil file createnew cpp.hxml 0
+  fsutil file createnew nodejs.hxml 0
   
-  echo --class-path src>>cpp.hxml
-  echo --cpp ../bin/cpp/Server.exe
-  echo --main Main
+  echo --class-path src>>nodejs.hxml
+  echo --cpp ../bin/cpp/Server.exe>>nodejs.hxml
+  echo -L hxnodejs>>nodejs.hxml
+  echo --main Main>>nodejs.hxml
   
-  haxe cpp.hxml
+  haxe nodejs.hxml
 )
