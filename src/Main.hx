f77d94b1f71b06a4f4cf484d14aea519b033488d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-05 14:57:22 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,17 @@
 package;
 
 class Main {
-  public static function main():Void {
-    trace("Begin");
+  static function main() {
+    // Configure our HTTP server to respond with Hello World to all requests.
+    var server = js.node.Http.createServer(function(request, response) {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.end("Hello World\n");
+    });
+
+    // Listen on port 8000, IP defaults to 127.0.0.1
+    server.listen(8000);
+
+    // Put a console.log on the terminal
+    trace("Server running at 127.0.0.1:8000");
   }
 }
