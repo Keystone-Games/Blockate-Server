f29ed7338d5905eb82c13fab9397d0cc69b97387
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-06 08:06:49 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -9,9 +9,9 @@ if %1==nodejs (
   fsutil file createnew nodejs.hxml 0
   
   echo --class-path src>>nodejs.hxml
-  echo --cpp ../bin/cpp/Server.exe>>nodejs.hxml
-  echo -L hxnodejs>>nodejs.hxml
-  echo --main Main>>nodejs.hxml
+  echo -lib hxnodejs>>nodejs.hxml
+  echo -main Main>>nodejs.hxml
+  echo -js ../bin/js/Server.js
   
   haxe nodejs.hxml
 )
