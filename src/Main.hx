0e8f2f3a36b990d0d04a7feb4deeea847c57f626
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-06 13:07:20 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -25,19 +25,39 @@ class Main {
     if (!FileSystem.isDirectory(cp)) { FileSystem.createDirectory(cp); }
     if (!FileSystem.exists(cf)) { File.saveContent(cf, JSON.stringify(defaultCF)); }
     
+    // load config file
+    
+    var conf = File.getContent(cf);
+    
+    // load world
+    
+    var wf = Path.join([cp, "world.json"]);
+    
+    var defaultWF = {
+      
+      mode: 0,
+      
+      worldName: "New World",
+    
+    };
+    
+    if (!FileSystem.exists(wf)) { File.saveContent(wf, JSON.stringify(defaultWF)); }
+    
+    var world = File.getContent(wf);
+    
     // Configure our HTTP server to respond with Hello World to all requests.
     var server = js.node.Http.createServer(function(request, response) {
       response.writeHead(200, {"Content-Type": "text/plain"});
       // response.end("Hello World\n");
       switch (request) {
         case "worldInfo":
-          response.end("test");
+          response.end(wf);
           break;
       }
     });
 
     // Listen on port 8000, IP defaults to 127.0.0.1
-    server.listen(8000);
+    server.listen(conf.port);
 
     // Put a console.log on the terminal
     trace("Server running at 127.0.0.1:8000");
