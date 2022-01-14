e7316548cfe4adedb807fb06f1115ff0f90811ac
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-01-14 14:33:39 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,11 +1,16 @@
 package;
 
+import haxe.io.Path;
+import haxe.Json;
+import sys.io.File;
+import sys.FileSystem;
+import js.node.Http;
+
 class Main {
 	static function main() {
 		#if !sys
 		return Sys.stderr().writeString("No file system available!\n");
 		#end
-
 		trace("Starting Blockate Server 0.0.1!");
 
 		// Configure the server storage files
@@ -17,9 +22,7 @@ class Main {
 		var defaultCF = {
 			ip: "127.0.0.1",
 			port: 8000,
-
 			pps: 167,
-
 			serverName: "Example",
 			serverDesc: "This is the default configuration for the Blockate server."
 		};
@@ -28,12 +31,11 @@ class Main {
 			FileSystem.createDirectory(cp);
 		}
 		if (!FileSystem.exists(cf)) {
-			File.saveContent(cf, JSON.stringify(defaultCF));
+			File.saveContent(cf, Json.stringify(defaultCF));
 		}
-
 		// load config file
 
-		var conf = File.getContent(cf);
+		var conf = Json.parse(File.getContent(cf));
 
 		// load world
 
@@ -41,18 +43,16 @@ class Main {
 
 		var defaultWF = {
 			mode: 0,
-
 			worldName: "New World",
 		};
 
 		if (!FileSystem.exists(wf)) {
-			File.saveContent(wf, JSON.stringify(defaultWF));
+			File.saveContent(wf, Json.stringify(defaultWF));
 		}
-
 		var world = File.getContent(wf);
 
 		// Configure our HTTP server to respond with Hello World to all requests.
-		var server = js.node.Http.createServer(function(request, response) {
+		var server = Http.createServer(function(request, response) {
 			response.writeHead(200, {"Content-Type": "text/plain"});
 			// response.end("Hello World\n");
 			response.end(wf, cf);
@@ -60,7 +60,6 @@ class Main {
 
 		// Listen on port 8000, IP defaults to 127.0.0.1
 		server.listen(conf.port);
-
 		// Put a console.log on the terminal
 		trace('Server running at ${conf.ip}:${conf.port}');
 	}
